10fb2973b2d72301824a74f59305fcfbb4e8b59b
Ernest Ernest <130362988+ernest-jere@users.noreply.github.com>
2024-09-20 17:33:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,44 @@
-let price = 19.5;
-let cid = 
-  [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]
-; 
-let cash = document.getElementById("cash");
-const purchaseBtn = document.getElementById("purchase-btn");
-const changeDue = document.getElementById("change-due")
+const authorContainer = document.getElementById('author-container');
+const loadMoreBtn = document.getElementById('load-more-btn');
 
-purchaseBtn.addEventListener("click", () => {
-  if (Number(cash.value) < price) {
-    alert("Customer does not have enough money to purchase the item");
+let startingIndex = 0;
+let endingIndex = 8;
+let authorDataArr = [];
+
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
+  .then((res) => res.json())
+  .then((data) => {
+    authorDataArr = data;
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));  
+  })
+  .catch((err) => {
+   authorContainer.innerHTML = '<p class="error-msg">There was an error loading the authors</p>';
+  });
+
+const fetchMoreAuthors = () => {
+  startingIndex += 8;
+  endingIndex += 8;
+
+  displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+  if (authorDataArr.length <= endingIndex) {
+    loadMoreBtn.disabled = true;
+loadMoreBtn.style.cursor = "not-allowed";
+    loadMoreBtn.textContent = 'No more data to load';
   }
-  if (Number(cash.value) === price) {
-    changeDue.textContent = "No change due - customer paid with exact cash";
-  }
-  if (Number(cash.value) > price) {
-    changeDue.textContent = CheckTransactionStatus(price, cash, cid);
-  } 
-})
+};
 
-function CheckTransactionStatus(price, cash, cid) {
-  const currencyUnit = [
-    ["PENNY", 0.01],
-    ["NICKEL", 0.05],
-    ["DIME", 0.1],
-    ["QUARTER", 0.25],
-    ["ONE", 1.0],
-    ["FIVE", 5.0],
-    ["TEN", 10.0],
-    ["TWENTY", 20.0],
-    ["ONE HUNDRED", 100.0]
-  ];
+const displayAuthors = (authors) => {
+  authors.forEach(({ author, image, url, bio }, index) => {
+    authorContainer.innerHTML += `
+    <div id="${index}" class="user-card">
+      <h2 class="author-name">${author}</h2>
+      <img class="user-img" src="${image}" alt="${author} avatar">
+      <div class="purple-divider"></div>
+      <p class="bio">${bio.length > 50 ? bio.slice(0, 50) + '...' : bio}</p>
+      <a class="author-link" href="${url}" target="_blank">${author} author page</a>
+    </div>
+  `;
+  });
+};
 
-  let change = Number(cash.value) - price;
-  let changeArray = [];
-
-  for (let i = currencyUnit.length - 1; i >= 0; i--) {
-    let coinName = currencyUnit[i][0];
-    let coinValue = currencyUnit[i][1];
-    let coinAmount = 0;
-
-    while (change >= coinValue && cid[i][1] > 0) {
-      change -= coinValue;
-      cid[i][1] -= coinValue;
-      coinAmount += coinValue;
-      change = Math.round(change * 100) / 100; 
-    }
-
-    if (coinAmount > 0) {
-      changeArray.push([coinName + ":", "$" + parseFloat(coinAmount).toFixed(2) + " "].join(" "));
-    }
-  }
-
-  if (change > 0) {
-    return  `status: INSUFFICIENT_FUNDS ${[]}` ;
-  }
-
-  if (cid.reduce((total, item) => total + item[1] === change)) {
-    return  `status: CLOSED ${changeArray}` ;
-  }
-
-  return  `Status: OPEN ${changeArray.join(" ")}` ;
-}
+loadMoreBtn.addEventListener('click', fetchMoreAuthors);
